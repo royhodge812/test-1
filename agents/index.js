71aca7468270ae189f652d06a5c5f6d93d468d8e
Roy Hodge Jr. <royhodge812@gmail.com>
2026-05-23 03:27:39 -0400

    Create agents module exports

---
+++ agents/index.js
@@ -0,0 +1,11 @@
+/**
+ * Agent Workers Module
+ * 
+ * Centralized exports for all available agent implementations.
+ */
+
+const { GeminiAgent } = require('./GeminiAgent');
+
+module.exports = {
+  GeminiAgent,
+};
